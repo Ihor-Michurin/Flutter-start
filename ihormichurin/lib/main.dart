75b8262cd5115d303da6fb5c1722e612141cf1a6
Ihor Michurin <imichurin@teamworkcommerce.com>
2023-03-12 00:07:56 +0200

    Fix spaces

--- ihormichurin/lib/main.dart
+++ ihormichurin/lib/main.dart
@@ -17,165 +17,171 @@ class InstagramClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
         leading: Icon(Icons.camera_alt_outlined),
-    title: Image.network(
-    'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png',
-    height: 40.0,
-    ),
-    actions: [
-    IconButton(
-    icon: Icon(Icons.live_tv),
-    onPressed: () {},
-    ),
-    IconButton(
-    icon: Icon(Icons.send),
-    onPressed: () {},
-    ),
-    ],
-    ),
-    body: Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-    Expanded(
-    child: ListView.builder(
-    itemCount: 10,
-    itemBuilder: (BuildContext context, int index) {
-    return Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-    // Post header
-    Container(
-    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    Row(
-    children: [
-    CircleAvatar(
-    backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=$index'),
-    radius: 16.0,
-    ),
-    SizedBox(width: 8.0),
-    Text(
-    'Username',
-    style: TextStyle(fontWeight: FontWeight.bold),
-    ),
-    ],
-    ),
-    IconButton(
-    icon: Icon(Icons.more_vert),
-    onPressed: () {},
-    ),
-    ],
-    ),
-    ),
-    // Post image
-    Image.network(
-    'https://picsum.photos/id/$index/400',
-    fit: BoxFit.cover,
-    height: 350.0,
-    ),
-    // Post actions
-    Container(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    Row(
-    children: [
-    IconButton(
-    icon: Icon(Icons.favorite_border),
-    onPressed: () {},
-    ),
-    SizedBox(width: 8.0),
-    IconButton(
-    icon: Icon(Icons.chat_bubble_outline),
-    onPressed: () {},
-    ),
-    SizedBox(width: 8.0),
-    IconButton(
-    icon: Icon(Icons.send),
-    onPressed: () {},
-    ),
-    ],
-    ),
-    IconButton(
-    icon: Icon(Icons.bookmark_border),
-    onPressed: () {},
-    ),
-    ],
-    ),
-    ),
-    // Post likes
-    Container(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Text(
-    '1,234 likes',
-    style: TextStyle(fontWeight: FontWeight.bold),
-    ),
-    ),
-    // Post caption
-    Container(
-    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    child: RichText(
-    text: TextSpan(
-    style: TextStyle(color: Colors.black),
-    children: [
-    TextSpan(
-    text: 'Username',
-    style: TextStyle(fontWeight: FontWeight.bold),
-    ),
-    TextSpan(text: ' '),
-    TextSpan(
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nisi quis enim ultrices suscipit.',
-    ),
-    ],
-    ),
-    ),
-    ),
-    // Post comments
-    Container(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    CircleAvatar(
-    backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=$index'),
-      radius: 16.0,
-    ),
-      SizedBox(width: 8.0),
-      Expanded(
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Add a comment...',
-            border: InputBorder.none,
+        title: Image.network(
+          'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png',
+          height: 40.0,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.live_tv),
+            onPressed: () {},
           ),
-        ),
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: () {},
+          ),
+        ],
       ),
-    ],
-    ),
-    ),
-      // Post timestamp
-      Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-        child: Text(
-          '10 minutes ago',
-          style: TextStyle(color: Colors.grey),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Post header
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://i.pravatar.cc/150?img=$index'),
+                                radius: 16.0,
+                              ),
+                              SizedBox(width: 8.0),
+                              Text(
+                                'Username',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.more_vert),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Post image
+                    Image.network(
+                      'https://picsum.photos/id/$index/400',
+                      fit: BoxFit.cover,
+                      height: 350.0,
+                    ),
+                    // Post actions
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.favorite_border),
+                                onPressed: () {},
+                              ),
+                              SizedBox(width: 8.0),
+                              IconButton(
+                                icon: Icon(Icons.chat_bubble_outline),
+                                onPressed: () {},
+                              ),
+                              SizedBox(width: 8.0),
+                              IconButton(
+                                icon: Icon(Icons.send),
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.bookmark_border),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Post likes
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        '1,234 likes',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    // Post caption
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Username',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: ' '),
+                            TextSpan(
+                              text:
+                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nisi quis enim ultrices suscipit.',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // Post comments
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://i.pravatar.cc/150?img=$index'),
+                            radius: 16.0,
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Add a comment...',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Post timestamp
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+                      child: Text(
+                        '10 minutes ago',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    // Divider between posts
+                    Divider(),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      // Divider between posts
-      Divider(),
-    ],
-    );
-    },
-    ),
-    ),
-    ],
-    ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
         selectedItemColor: Colors.black,
