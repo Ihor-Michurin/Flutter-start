22ca9e409acb8562918635d999dc0f62cefea55b
Ihor Michurin <imichurin@teamworkcommerce.com>
2023-03-11 23:49:13 +0200

    Add l3

--- ihormichurin/lib/main.dart
+++ ihormichurin/lib/main.dart
@@ -1,74 +1,211 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp.withFullName('Ihor Michurin'));
+  runApp(MyApp());
 }
 
-mixin Greetable {
-  void greet() {
-    print('Hello!');
-  }
-}
-
-class Person {
-  Person({required this.firstName, required this.lastName});
-
-  String firstName;
-  String lastName;
-}
-
-class MyApp extends StatelessWidget with Greetable {
-  final Person person = Person(firstName: 'Ihor', lastName: 'Michurin')..lastName ??= 'Michurin';
-
-  MyApp({this.firstName = '', this.lastName = ''});
-
-  factory MyApp.fromDefault() {
-    return MyApp();
-  }
-
-  factory MyApp.withFullName(String fullName, {String? firstName, String? lastName}) {
-    final names = fullName.split(' ');
-    return MyApp(
-      firstName: firstName ?? names[0],
-      lastName: lastName ?? (names.length > 1 ? names[1] : ''),
-    );
-  }
-
-  final String firstName;
-  final String lastName;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final list = [1, 2, 3, 4, 5];
-    final map = {'first': 1, 'second': 2, 'third': 3};
-
-    assert(list.isNotEmpty, 'List is empty');
-    assert(map.isNotEmpty, 'Map is empty');
-
     return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
+      home: InstagramClone(),
+    );
+  }
+}
+
+class InstagramClone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('First Name: ${person.firstName}'),
-              Text('Last Name: ${person.lastName}'),
-              Text('Input First Name: $firstName'),
-              Text('Input Last Name: $lastName'),
-              ElevatedButton(
-                onPressed: () {
-                  list.add(6);
-                  map['fourth'] = 4;
-                },
-                child: Text('Add to List and Map'),
-              ),
-            ],
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: Icon(Icons.camera_alt_outlined),
+    title: Image.network(
+    'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png',
+    height: 40.0,
+    ),
+    actions: [
+    IconButton(
+    icon: Icon(Icons.live_tv),
+    onPressed: () {},
+    ),
+    IconButton(
+    icon: Icon(Icons.send),
+    onPressed: () {},
+    ),
+    ],
+    ),
+    body: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+    Expanded(
+    child: ListView.builder(
+    itemCount: 10,
+    itemBuilder: (BuildContext context, int index) {
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+    // Post header
+    Container(
+    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+    Row(
+    children: [
+    CircleAvatar(
+    backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=$index'),
+    radius: 16.0,
+    ),
+    SizedBox(width: 8.0),
+    Text(
+    'Username',
+    style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    ],
+    ),
+    IconButton(
+    icon: Icon(Icons.more_vert),
+    onPressed: () {},
+    ),
+    ],
+    ),
+    ),
+    // Post image
+    Image.network(
+    'https://picsum.photos/id/$index/400',
+    fit: BoxFit.cover,
+    height: 350.0,
+    ),
+    // Post actions
+    Container(
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+    Row(
+    children: [
+    IconButton(
+    icon: Icon(Icons.favorite_border),
+    onPressed: () {},
+    ),
+    SizedBox(width: 8.0),
+    IconButton(
+    icon: Icon(Icons.chat_bubble_outline),
+    onPressed: () {},
+    ),
+    SizedBox(width: 8.0),
+    IconButton(
+    icon: Icon(Icons.send),
+    onPressed: () {},
+    ),
+    ],
+    ),
+    IconButton(
+    icon: Icon(Icons.bookmark_border),
+    onPressed: () {},
+    ),
+    ],
+    ),
+    ),
+    // Post likes
+    Container(
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    child: Text(
+    '1,234 likes',
+    style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    ),
+    // Post caption
+    Container(
+    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: RichText(
+    text: TextSpan(
+    style: TextStyle(color: Colors.black),
+    children: [
+    TextSpan(
+    text: 'Username',
+    style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    TextSpan(text: ' '),
+    TextSpan(
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nisi quis enim ultrices suscipit.',
+    ),
+    ],
+    ),
+    ),
+    ),
+    // Post comments
+    Container(
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    child: Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    CircleAvatar(
+    backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=$index'),
+      radius: 16.0,
+    ),
+      SizedBox(width: 8.0),
+      Expanded(
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Add a comment...',
+            border: InputBorder.none,
           ),
         ),
+      ),
+    ],
+    ),
+    ),
+      // Post timestamp
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+        child: Text(
+          '10 minutes ago',
+          style: TextStyle(color: Colors.grey),
+        ),
+      ),
+      // Divider between posts
+      Divider(),
+    ],
+    );
+    },
+    ),
+    ),
+    ],
+    ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Likes',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=10'),
+              radius: 14.0,
+            ),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
