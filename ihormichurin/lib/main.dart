d52583c532f39348923f47b73f1f95439b5aa187
Ihor Michurin <imichurin@teamworkcommerce.com>
2023-04-22 22:21:43 +0300

    Add l9

--- ihormichurin/lib/main.dart
+++ ihormichurin/lib/main.dart
@@ -1,115 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Ihor Michurin'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AnimatedBarChart extends StatefulWidget {
+  const AnimatedBarChart({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AnimatedBarChartState createState() => _AnimatedBarChartState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AnimatedBarChartState extends State<AnimatedBarChart>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  final List<double> _data = [0.2, 0.4, 0.6, 0.8];
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..addListener(() {
+      setState(() {});
     });
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Animated Bar Chart'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SizedBox(
+          width: 300,
+          height: 200,
+          child: CustomPaint(
+            painter: _BarChartPainter(_data, _animation.value),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          setState(() {
+            _currentIndex = (_currentIndex + 1) % _data.length;
+            _data[_currentIndex] = 0.2 + 0.6 * _animation.value;
+          });
+        },
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+}
+
+class _BarChartPainter extends CustomPainter {
+  final List<double> data;
+  final double animationValue;
+
+  _BarChartPainter(this.data, this.animationValue);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final barWidth = size.width / data.length;
+    final barHeight = size.height * animationValue;
+    final paint = Paint();
+
+    for (int i = 0; i < data.length; i++) {
+      final x = i * barWidth;
+      final y = size.height * (1 - data[i]);
+
+      paint.color = Colors.blue.shade400;
+      if (i % 2 == 1) {
+        paint.color = Colors.red.shade400;
+      }
+
+      canvas.drawRect(
+        Rect.fromLTWH(x, y, barWidth, barHeight),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(_BarChartPainter oldDelegate) =>
+      animationValue != oldDelegate.animationValue;
+}
+
+void main() {
+  runApp(const MaterialApp(home: AnimatedBarChart()));
 }
